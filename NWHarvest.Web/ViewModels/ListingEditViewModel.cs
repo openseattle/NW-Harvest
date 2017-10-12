fee2d28c005d9d0e96111bcef33ddf24b9c310c5
Adrian Limon <me@adrianlimon.com>
2017-10-11 23:23:55 -0700

    refactor ListingsController and views

---
+++ NWHarvest.Web/ViewModels/ListingEditViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NWHarvest.Web.ViewModels
+{
+    public class ListingEditViewModel : ListingViewModel
+    {
+        [Required]
+        public override int Id { get; set; }
+
+        [Required]
+        public override int PickupLocationId { get; set; }
+
+        [Required]
+        public override string Product { get; set; }
+
+        [Required]
+        public override decimal QuantityAvailable { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public override string UnitOfMeasure { get; set; }
+
+        [Required]
+        public override DateTime ExpirationDate { get; set; }
+
+        [Required]
+        public override decimal CostPerUnit { get; set; }
+    }
+}
