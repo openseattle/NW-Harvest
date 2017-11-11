6f3d32ae345e70494190a1b4ad537bed03824088
Ankit Dwivedi <adwivedi@microsoft.com>
2017-11-10 21:49:00 -0800

    Rename RoleEditViewModel and use AddressEditViewModel

--- NWHarvest.Web/Controllers/GrowersController.cs
+++ NWHarvest.Web/Controllers/GrowersController.cs
@@ -113,18 +113,21 @@ namespace NWHarvest.Web.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var vm = db.Growers.Select(g => new RoleEditViewModel
+            var vm = db.Growers.Select(g => new GrowerEditViewModel
             {
                 Id = g.Id,
                 Name = g.name,
-                Address1 = g.address1,
-                Address2 = g.address2,
-                Address3 = g.address3,
-                Address4 = g.address4,
-                City = g.city,
-                County = g.county,
-                State = g.state,
-                Zip = g.zip,
+                Address = new AddressEditViewModel
+                {
+                    Address1 = g.address1,
+                    Address2 = g.address2,
+                    Address3 = g.address3,
+                    Address4 = g.address4,
+                    City = g.city,
+                    County = g.county,
+                    State = g.state,
+                    Zip = g.zip
+                },
                 IsActive = g.IsActive,
                 NotificationPreference = g.NotificationPreference
             })
@@ -144,20 +147,20 @@ namespace NWHarvest.Web.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Grower")]
-        public ActionResult Edit(RoleEditViewModel vm)
+        public ActionResult Edit(GrowerEditViewModel vm)
         {
             if (ModelState.IsValid)
             {
                 var grower = db.Growers.Find(vm.Id);
                 grower.name = vm.Name;
-                grower.address1 = vm.Address1;
-                grower.address2 = vm.Address2;
-                grower.address3 = vm.Address3;
-                grower.address4 = vm.Address4;
-                grower.city = vm.City;
-                grower.county = vm.County;
-                grower.state = vm.State;
-                grower.zip = vm.Zip;
+                grower.address1 = vm.Address.Address1;
+                grower.address2 = vm.Address.Address2;
+                grower.address3 = vm.Address.Address3;
+                grower.address4 = vm.Address.Address4;
+                grower.city = vm.Address.City;
+                grower.county = vm.Address.County;
+                grower.state = vm.Address.State;
+                grower.zip = vm.Address.Zip;
                 grower.IsActive = vm.IsActive;
                 db.SaveChanges();
 
---
+++ NWHarvest.Web/ViewModels/GrowerEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NWHarvest.Web.ViewModels
+{
+    public class GrowerEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public AddressEditViewModel Address { get; set; }
+
+        [Required]
+        [Display(Name = "Active")]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public string NotificationPreference { get; set; }
+    }
+}
