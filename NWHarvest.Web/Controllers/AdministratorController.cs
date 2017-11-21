20211cd3fb3dba8fc81c9ddc290ec3f13dc76dd3
Adrian Limon <me@adrianlimon.com>
2017-11-21 14:00:01 -0800

    add manage growers

--- NWHarvest.Web/Controllers/AdministratorController.cs
+++ NWHarvest.Web/Controllers/AdministratorController.cs
@@ -1,4 +1,7 @@
-﻿using NWHarvest.Web.Models;
+﻿using NWHarvest.Web.Enums;
+using NWHarvest.Web.Models;
+using NWHarvest.Web.ViewModels;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace NWHarvest.Web.Controllers
@@ -7,11 +10,49 @@ namespace NWHarvest.Web.Controllers
     public class AdministratorController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
+        private const int DAY_LIMIT_FOR_ADMINISTRATORS = 180;
+
 
         [Authorize(Roles = "Administrator")]
         public ActionResult Index()
         {
-            return View();
+            var vm = new AdministratorViewModel();
+            vm.NumberOfGrowers = db.Growers.Count();
+
+            return View(vm);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult ManageUser(UserRole userRole)
+        {
+            switch (userRole)
+            {
+                case UserRole.Administrator:
+                    break;
+                case UserRole.Grower:
+                    return RedirectToAction(nameof(ManageGrowers));
+                case UserRole.FoodBank:
+                    break;
+                default:
+                    break;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult ManageGrowers()
+        {
+            var vm = db.Growers
+                .OrderBy(g => g.name)
+                .Select(g => new GrowerViewModel
+                {
+                    Id = g.Id,
+                    Name = g.name,
+                    IsActive = g.IsActive
+                }).ToList();
+
+            return View(vm);
         }
     }
 }
---
+++ NWHarvest.Web/ViewModels/AdministratorViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NWHarvest.Web.ViewModels
+{
+    public class AdministratorViewModel
+    {
+        [Display(Name = "Users")]
+        public int NumberOfUsers {
+            get => NumberOfGrowers + NumberOfFoodBanks;
+        }
+
+        [Display(Name = "Growers")]
+        public int NumberOfGrowers { get; set; }
+
+        [Display(Name = "FoodBanks")]
+        public int NumberOfFoodBanks { get; set; }
+
+        [Display(Name = "Listings")]
+        public int NumberOfListings { get; set; }
+
+        [Display(Name = "Available Listings")]
+        public int NumberOfAvailableListings { get; set; }
+
+        [Display(Name = "Pending Pickups")]
+        public int NumberOfPendingPickupClaimListings { get; set; }
+
+        [Display(Name = "Claimed Listings")]
+        public int NumberOfClaimedListings { get; set; }
+
+        [Display(Name = "Expired Listings")]
+        public int NumberOfExpiredListings { get; set; }
+        
+        public ICollection<ListingViewModel> Listings { get; set; }
+    }
+}
