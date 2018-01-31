7f6f7e5b63e950dc27ebfd38b86eac318e5f0268
Adrian Limon <me@adrianlimon.com>
2018-01-31 14:07:16 -0800

    add administrator listing view model

---
+++ NWHarvest.Web/ViewModels/AdministratorListingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using NWHarvest.Web.Models;
+
+namespace NWHarvest.Web.ViewModels
+{
+    public class AdministratorListingViewModel
+    {
+        public virtual int Id { get; set; }
+        
+        [Required]
+        public virtual string Product { get; set; }
+
+        [Required]
+        [DisplayName("Available Qty")]
+        public virtual decimal AvailableQuantity { get; set; }
+
+        [DisplayName("Claimed Qty")]
+        public decimal ClaimedQuantity { get; set; }
+        
+        [DisplayName("Expires")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public virtual DateTime ExpirationDate { get; set; }
+                
+        public Grower Grower { get; set; }
+        public FoodBank FoodBank { get; set; }
+        public ListerViewModel Lister { get; set; }
+    }
+}
